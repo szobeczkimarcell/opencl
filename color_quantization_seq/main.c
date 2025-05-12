5a13804e5009ac8f734a45881e918506738ddaff
szobeczkimarcell <113781206+szobeczkimarcell@users.noreply.github.com>
2025-05-13 00:35:30 +0200

    Add files via upload

--- color_quantization_seq/main.c
+++ color_quantization_seq/main.c
@@ -1,141 +1,192 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include "include/stb_image.h"
-
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "include/stb_image.h"
 #include "include/stb_image_write.h"
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <math.h>
+#include <time.h>
 
-typedef struct {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
+#define MAX_ITERATIONS 100
+
+typedef struct{
+    float r, g, b;
 } Color;
 
+static inline float dist2(float r1, float g1, float b1, float r2, float g2, float b2) {
+    float dr = r1 - r2;
+    float dg = g1 - g2;
+    float db = b1 - b2;
+    return dr*dr + dg*dg + db*db;
+}
 
-int read_palette(const char* palette_filename, Color** palette) {
-    FILE* fp = fopen(palette_filename, "r");
+int is_number(const char *s) {
+    if (!*s) return 0;
+    while (*s) {
+        if (*s < '0' || *s > '9') return 0;
+        s++;
+    }
+    return 1;
+}
+
+Color *load_palette(const char *paletteFile, int *paletteSizeOut) {
+    FILE *fp = fopen(paletteFile, "r");
     if (!fp) {
-        fprintf(stderr, "Error: Unable to open palette file '%s'\n", palette_filename);
-        return 0;
+        fprintf(stderr, "Failed to open palette file");
+        exit(EXIT_FAILURE);
     }
 
-    char line[128];
-    int color_count = 0;
-    int capacity = 10;
-
-    *palette = (Color*)malloc(capacity * sizeof(Color));
-    if (!(*palette)) {
-        fprintf(stderr, "Error: Could not allocate memory for palette.\n");
-        fclose(fp);
-        return 0;
+    int capacity = 1024;
+    Color *palette = malloc(capacity * sizeof(Color));
+    if (!palette) {
+        fprintf(stderr, "Out of memory\n");
+        exit(EXIT_FAILURE);
     }
 
+    int count = 0;
+    char line[256];
     while (fgets(line, sizeof(line), fp)) {
-        if (line[0] == '\n' || line[0] == '\0') {
-            continue;
-        }
-
+        if (line[0] != '#') continue;
         unsigned int r, g, b;
-        if (sscanf(line, "#%02x%02x%02x", &r, &g, &b) == 3) {
-            if (color_count >= capacity) {
-                capacity += 5;
-                Color* temp = (Color*)realloc(*palette, capacity * sizeof(Color));
-                if (!temp) {
-                    fprintf(stderr, "Error: Could not reallocate memory.\n");
-                    fclose(fp);
-                    free(*palette);
-                    return 0;
-                }
-                *palette = temp;
-            }
+        if (sscanf(line + 1, "%02x%02x%02x", &r, &g, &b) != 3) continue;
 
-            (*palette)[color_count].r = (unsigned char)r;
-            (*palette)[color_count].g = (unsigned char)g;
-            (*palette)[color_count].b = (unsigned char)b;
+        palette[count].r = (float)r;
+        palette[count].g = (float)g;
+        palette[count].b = (float)b;
+        printf("Color %d: R:%.0f, G:%.0f, B:%.0f\n", count+1, palette[count].r, palette[count].g, palette[count].b);
 
-            color_count++;
+        if (++count >= capacity) {
+            fprintf(stderr, "Palette too large, max %d\n", capacity);
+            break;
         }
     }
-
     fclose(fp);
-    return color_count;
-}
 
-static inline Color nearest_color(unsigned char r, unsigned char g, unsigned char b,
-                                  Color* palette, int palette_size) {
-    double min_dist = (double)INT_MAX;
-    Color nearest = {0, 0, 0};
-
-    for (int i = 0; i < palette_size; i++) {
-        double dr = (double)r - palette[i].r;
-        double dg = (double)g - palette[i].g;
-        double db = (double)b - palette[i].b;
-        double dist = dr*dr + dg*dg + db*db;
-        if (dist < min_dist) {
-            min_dist = dist;
-            nearest = palette[i];
+    if (count < capacity) {
+        palette = realloc(palette, count * sizeof(Color));
+        if (!palette && count > 0) {
+            fprintf(stderr, "Realloc failed\n");
+            exit(EXIT_FAILURE);
         }
     }
 
-    return nearest;
+    *paletteSizeOut = count;
+    return palette;
 }
 
-int main(int argc, char** argv) {
+
+Color *kmeans_palette(unsigned char *img, int w, int h, int k, int max_iter) {
+    int npix = w*h;
+    Color *centroids = malloc(k * sizeof *centroids);
+    int *labels = malloc(npix * sizeof *labels);
+    // Select k random centroids
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < k; i++) {
+        int index = rand() % npix;
+        centroids[i].r = img[4*index+0];
+        centroids[i].g = img[4*index+1];
+        centroids[i].b = img[4*index+2];
+    }
+    // More iterations = convergent results
+    for (int it = 0; it < max_iter; it++) {
+        int changed = 0;
+        // Assign every pixel to their closest centroid
+        for (int i = 0; i < npix; i++) {
+            float pr = img[4*i+0], pg = img[4*i+1], pb = img[4*i+2];
+            int best = 0;
+            float bd = dist2(pr,pg,pb, centroids[0].r,centroids[0].g,centroids[0].b);
+            for (int j = 1; j < k; j++) {
+                float d = dist2(pr,pg,pb, centroids[j].r,centroids[j].g,centroids[j].b);
+                if (d < bd) { 
+                    bd = d;
+                    best = j;
+                }
+            }
+            if (labels[i] != best) { 
+                labels[i] = best;
+                changed++;
+            }
+        }
+        if (!changed) break;
+        // Move centroids closer to average
+        long *sumr = calloc(k, sizeof *sumr);
+        long *sumg = calloc(k, sizeof *sumg);
+        long *sumb = calloc(k, sizeof *sumb);
+        int *cnt = calloc(k, sizeof *cnt);
+        for (int i = 0; i < npix; i++) {
+            int c = labels[i];
+            sumr[c] += img[4*i+0];
+            sumg[c] += img[4*i+1];
+            sumb[c] += img[4*i+2];
+            cnt[c]++;
+        }
+        for (int j = 0; j < k; j++) {
+            if (cnt[j]) {
+                centroids[j].r = sumr[j] / (float)cnt[j];
+                centroids[j].g = sumg[j] / (float)cnt[j];
+                centroids[j].b = sumb[j] / (float)cnt[j];
+            }
+        }
+        free(sumr);
+        free(sumg);
+        free(sumb);
+        free(cnt);
+    }
+    free(labels);
+    for (int i = 0; i < k; i++){
+        printf("Color %d: R: %.00f | G: %.00f | B:%.00f\n", i+1, centroids[i].r, centroids[i].g, centroids[i].b);
+    }
+    return centroids;
+}
+
+int main(int argc, char **argv) {
     if (argc < 4) {
-        fprintf(stderr, "Usage: %s <palette.txt> <input_image> <output_image>\n", argv[0]);
-        return 1;
+        fprintf(stderr, "Usage: %s <palette.txt OR number> <input_image> <output_image>\n", argv[0]);
+        return EXIT_FAILURE;
     }
-
-    const char* palette_filename = argv[1];
-    const char* input_filename = argv[2];
-    const char* output_filename = argv[3];
-
-    /* Read palette from file */
-    Color* palette = NULL;
-    int palette_size = read_palette(palette_filename, &palette);
-    if (palette_size == 0) {
-        fprintf(stderr, "Error: No colors read from palette or file not found.\n");
-        return 1;
+    const char *p = argv[1];
+    const char *infile = argv[2];
+    const char *outfile = argv[3];
+    int w, h, comp;
+    unsigned char *img = stbi_load(infile, &w, &h, &comp, 4);
+    if (!img) exit(1);
+    int npix = w*h;
+    Color *palette;
+    int pn;
+    // Start clock
+    clock_t start = clock();
+    if (is_number(p)) {
+        int k = atoi(p);
+        if (k < 1) exit(1);
+        palette = kmeans_palette(img, w, h, k, MAX_ITERATIONS);
+        pn = k;
+    } else {
+        palette = load_palette(p, &pn);
     }
-
-    int width, height, channels;
-    unsigned char* data = stbi_load(input_filename, &width, &height, &channels, 0);
-    if (!data) {
-        fprintf(stderr, "Error: Could not load image '%s'.\n", input_filename);
-        free(palette);
-        return 1;
+    unsigned char *out = malloc(npix * 4);
+    // Create new image from palette
+    for (int i = 0; i < npix; i++) {
+        float pr = img[4*i+0], pg = img[4*i+1], pb = img[4*i+2];
+        int best = 0;
+        float bd = dist2(pr,pg,pb, palette[0].r,palette[0].g,palette[0].b);
+        for (int j = 1; j < pn; j++) {
+            float d = dist2(pr,pg,pb, palette[j].r,palette[j].g,palette[j].b);
+            if (d < bd) {
+                bd = d;
+                best = j;
+            }
+        }
+        out[4*i+0] = (unsigned char)(palette[best].r);
+        out[4*i+1] = (unsigned char)(palette[best].g);
+        out[4*i+2] = (unsigned char)(palette[best].b);
+        out[4*i+3] = img[4*i+3];
     }
-
-    size_t pixel_count = (size_t)width * height;
-
-    for (size_t i = 0; i < pixel_count; i++) {
-        size_t idx = i * channels;
-
-        unsigned char r = data[idx + 0];
-        unsigned char g = data[idx + 1];
-        unsigned char b = data[idx + 2];
-        
-        Color c = nearest_color(r, g, b, palette, palette_size);
-
-        data[idx + 0] = c.r;
-        data[idx + 1] = c.g;
-        data[idx + 2] = c.b;
-    }
-
-    if (!stbi_write_png(output_filename, width, height, channels, data, width * channels)) {
-        fprintf(stderr, "Error: Failed to write image '%s'.\n", output_filename);
-        stbi_image_free(data);
-        free(palette);
-        return 1;
-    }
-
-    stbi_image_free(data);
+    stbi_write_png(outfile, w, h, 4, out, w*4);
+    free(img);
+    free(out);
     free(palette);
-
-    printf("Successfully wrote output to '%s'\n", output_filename);
+    // End clock
+    printf("Runtime: %.6f seconds\n", (double)(clock()-start)/CLOCKS_PER_SEC);
     return 0;
 }
